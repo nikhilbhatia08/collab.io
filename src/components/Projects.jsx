0adb83f48f09fcfcba95ceaba964899efbee1193
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-17 11:24:35 +0530

    made the projects page

--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -1,17 +1,30 @@
 import React from 'react'
-import Pagination from './Pagination'
+// import Pagination from './Pagination'
+import { Link } from 'react-router-dom'
 
 function Projects() {
+    const projects = [1,2,3,4,5];
   return (
     <>
         <div className="py-12">
-            <div className="mx-auto rounded-lg border max-w-7xl px-6 lg:px-8">
-                <h1 className="border-b-2 ml-1 py-4 text-white">projects</h1>
-                <div className="flex-col">
-                    <div className="flex-row flex-wrap justify-between">
-                        <img className="h-16 w-16 rounded-full my-10" src={require('../pictures/picofdev.png')} alt="" />
+            <div className="mx-auto rounded-lg border bg-slate-800 max-w-5xl px-6 lg:px-8">
+            <h1 className="border-b ml-1 py-4 text-white">projects</h1>
+            {projects.map((project)=>{
+                return <div>
+                <div className="flex-col border-b">
+                    <div className="flex flex-wrap items-center">
+                        <div className="inline-block"><img className="h-16 w-16 rounded-full my-10" src={require('../pictures/picofdev.png')} alt="" /></div>
+                        <div className="my-4 inline-block"><p className="mx-5 inline-block lg:text-3xl md:text-base sm:text-xs text-white">Name/Title of the project</p></div>
+                        <Link className="mx-auto" to='/sponsor'><div className="flex shadow-[0px_22px_22px__0px_rgba(31,_41,_234,_0.4)] hover:shadow-[0px_22px_22px__0px_rgba(31,_41,_234,_0.5)] mx-auto border-blue-600 bg-blue-500 rounded-md py-1 px-1">
+                            <div className="inline-block hover:scale-110"><img src={require('../pictures/heart.png')} alt="" /></div>
+                            <div className="inline-block text-white mx-1">Sponsor</div>
+                        </div></Link>
                     </div>
+                    <div className="my-2 text-white">Category - </div>
                 </div>
+                    </div>
+            })}
+            
             </div>
         </div>
     </>
