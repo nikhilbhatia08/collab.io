2cba94ad24020aafaffb98a9438a57125655e2ae
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-17 16:43:05 +0530

    change in project deep component

--- src/components/Inproj.jsx
+++ src/components/Inproj.jsx
@@ -14,11 +14,32 @@ function Inproj(props) {
         <div className="flex flex-wrap justify-center">
             <div className="mx-10 inline-block my-5 basis-3/4 self-start flex-col justify-center items-center rounded-lg border bg-slate-800 max-w-4xl px-6 lg:px-8">
                 <div><h1 className="text-white py-2 border-b lg:text-4xl md:text-base sm:text-xs">Readme</h1></div>
-                <div className="my-4"><img className="mx-auto lg:h-96 sm:h-40 md:h-24" src={require('../pictures/picofdev.png')} alt="" /></div>
+                <div className="border-b pb-4">
+                <div className="my-4"><img className="mx-auto self-start lg:h-96 sm:h-40 md:h-24" src={require('../pictures/picofdev.png')} alt="" /></div>
                 <div><p className="text-white md:text-xs sm:text:xs lg:text-2xl">This section the contains the problem statement or the purpose of the propject</p></div>
                 <div><p className="text-white md:text-xs sm:text:xs lg:text-2xl">This section contains the solution for the problem statement and the reason fro the project</p></div>
                 <div><p className="text-white md:text-xs sm:text:xs lg:text-2xl inline-block">Deployment Link - </p><Link className="mx-2 text-blue-500 underline">sggsa</Link></div>
                 <div><p className="text-white md:text-xs sm:text:xs lg:text-2xl inline-block">Name of the college or institution - </p><Link className="mx-2 shadow-[0px_22px_22px__0px_rgba(31,_41,_234,_0.4)] text-blue-500 underline">College Name and link</Link></div>
+                </div>
+                <div className="py-4 flex-col border-b">
+                    <div className=""><h1 className="text-white sm:text-xs md:text-xs lg:text-4xl">Video</h1></div>
+                    <div className="py-3 md-h-40"><iframe className="mx-auto lg:w-[540px] md-w-[200px] sm:w-[310px] lg:h-96 sm:h-[240px] md:h-40" 
+                        src="https://www.youtube.com/embed/WtkKwO1viI8?si=igjLzL4sI_08nHM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
+                    </iframe></div>
+                </div>
+                <div className="py-4 flex-col border-b">
+                    <div className=""><p className="text-4xl text-white">Architecture</p></div>
+                    <div className="my-4"><img className="mx-auto self-start lg:h-96 sm:h-40 md:h-24" src={require('../pictures/picofdev.png')} alt="" /></div>
+                    <div><p className="text-white">This will contain explaination about hte architecture and working od the project</p></div>
+                </div>
+                <div className="py-4 flex-col border-b">
+                    <div className=""><p className="text-4xl text-white">Liked Our work ? Would You Like to sponsor Us</p></div>
+                    <h1 className="text-white">Links of Sponsoring us will come here</h1>
+                </div>
+                <div className="py-4 flex-col border-b">
+                    <div className=""><p className="text-4xl text-white">Follow Us</p></div>
+                    <h1 className="text-white">=Link of Social media handle will come here</h1>
+                </div>
             </div>
             <div className="mx-10 my-5 inline-block self-start basis-1/4 flex-col rounded-lg border bg-slate-800 px-6">
                 <div className="border-b py-4">
@@ -29,7 +50,7 @@ function Inproj(props) {
                         <div className="w-12 h-12  inline-block">
                             <img className="rounded-full inline-block" src={require('../pictures/picofdev.png')} alt="" />
                         </div>
-                        <div className="inline-block"><p className="mx-4 inline-block text-white md:text:xl lg:text-2xl">Name</p></div>
+                        <Link to={`/user/idx`}><div className="inline-block"><p className="mx-4 hover:text-blue-500 inline-block text-white md:text:xl lg:text-2xl">Name</p></div></Link>
                     </div>
                     </div>:<div></div>;
                 })}
@@ -51,7 +72,7 @@ function Inproj(props) {
                         <div className="w-12 h-12  inline-block">
                             <img className="rounded-full inline-block" src={require('../pictures/picofdev.png')} alt="" />
                         </div>
-                        <div className="inline-block"><p className="mx-4 inline-block text-white md:text:xl lg:text-2xl">Name</p></div>
+                        <Link to={`/user/idx`}><div className="inline-block"><p className="mx-4 inline-block hover:text-blue-500 text-white md:text:xl lg:text-2xl">Name</p></div></Link>
                     </div>
                     </div>:<div></div>;
                 })}
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -14,7 +14,7 @@ function Projects() {
                 return <div>
                 <div className="flex-col border-b">
                     <div className="flex flex-wrap items-center">
-                        <div className="inline-block"><img className="h-16 w-16 rounded-full my-10" src={require('../pictures/picofdev.png')} alt="" /></div>
+                        <div className="inline-block"><img className="h-16 w-16 rounded-full my-5" src={require('../pictures/picofdev.png')} alt="" /></div>
                         <div className="my-4 inline-block"><Link to={`/projects/${projectsId}`}><p className="mx-5 inline-block hover:text-blue-500 lg:text-3xl md:text-base sm:text-xs text-white">Name/Title of the project</p></Link></div>
                         <Link className="mx-auto" to='/sponsor'><div className="flex shadow-[0px_22px_22px__0px_rgba(31,_41,_234,_0.4)] hover:shadow-[0px_22px_22px__0px_rgba(31,_41,_234,_0.5)] mx-auto border-blue-600 bg-blue-500 rounded-md py-1 px-1">
                             <div className="inline-block hover:scale-110"><img src={require('../pictures/heart.png')} alt="" /></div>
