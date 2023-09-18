b90c44fcf8c781ecdded06095cb9f651b6bf5058
KANABOINA VIGNESH <123869203+Vignesh18280@users.noreply.github.com>
2023-09-18 11:12:04 +0000

    enabled add items button to freelance page

--- src/components/GetFreelance.jsx
+++ src/components/GetFreelance.jsx
@@ -3,13 +3,19 @@ import { Link } from 'react-router-dom'
 
 function Getfreelance() {
   return (
-    <div class="bg-slate-750 py-24 sm:py-32">
+    <div class="bg-slate-750 py-10 sm:py-12">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:mx-0">
       <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Unlock the Door to Your <span className="text-blue-800" >Freelance Dreams</span></h2>
       <p class="mt-2 text-lg leading-8 text-gray-600">So, what are you waiting for?seize the opportunity, unleash your potential, and let your freelance journey begin with a single, electrifying choice here!</p>
     </div>
-    <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+    {/* add item */}
+    <div className=" flex justify-between mt-5 border-2 border-white p-3 rounded-md text-white">
+       <h1 className="font-bold mt-2 sm: 576px">Add your Project and engage proficient freelancers to execute it </h1>
+       <button className="bg-blue-800 p-2 pl-4 pr-4 border-2 border-white rounded-md allign-left"><Link to='#'>Add</Link></button>
+    </div>
+    <div class="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
+      {/* card starts from here */}
       <article class="flex max-w-xl flex-col border-2 border-white p-3 rounded-md items-start justify-between">
         <div class="flex items-center gap-x-4 text-xs">
           <time datetime="2020-03-16" class="text-gray-500">Sep 18, 2023</time>
