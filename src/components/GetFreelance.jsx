8714eab77732e27771971a5a4fe8b29d166d53c2
nikhilbhatia08 <bhatianikhil551@gmail.com>
2023-09-18 20:03:50 +0530

    change in freelance page

--- src/components/GetFreelance.jsx
+++ src/components/GetFreelance.jsx
@@ -6,17 +6,17 @@ function Getfreelance() {
     <div class="bg-slate-750 py-10 sm:py-12">
   <div class="mx-auto max-w-7xl px-6 lg:px-8">
     <div class="mx-auto max-w-2xl lg:mx-0">
-      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Unlock the Door to Your <span className="text-blue-800" >Freelance Dreams</span></h2>
-      <p class="mt-2 text-lg leading-8 text-gray-600">So, what are you waiting for?seize the opportunity, unleash your potential, and let your freelance journey begin with a single, electrifying choice here!</p>
+      <h2 class="text-3xl font-bold tracking-tight text-white sm:text-4xl">Unlock the Door to Your <span className="text-blue-500" >Freelance Dreams</span></h2>
+      <p class="mt-2 text-lg leading-8 text-gray-400">So, what are you waiting for?seize the opportunity, unleash your potential, and let your freelance journey begin with a single, electrifying choice here!</p>
     </div>
     {/* add item */}
-    <div className=" flex justify-between mt-5 border-2 border-white p-3 rounded-md text-white">
-       <h1 className="font-bold mt-2 sm: 576px">Add your Project and engage proficient freelancers to execute it </h1>
-       <button className="bg-blue-800 p-2 pl-4 pr-4 border-2 border-white rounded-md allign-left"><Link to='#'>Add</Link></button>
+    <div className=" flex justify-between mt-5 border p-3 rounded-md text-white">
+       <h1 className="text-2xl py-2 sm: 576px">Add your Project and engage proficient freelancers to execute it </h1>
+       <button className="bg-blue-500 p-2 pl-4 pr-4 rounded-md"><Link to='#'>Add</Link></button>
     </div>
     <div class="grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
       {/* card starts from here */}
-      <article class="flex max-w-xl flex-col border-2 border-white p-3 rounded-md items-start justify-between">
+      <article class="flex max-w-xl flex-col border  p-3 rounded-md items-start justify-between">
         <div class="flex items-center gap-x-4 text-xs">
           <time datetime="2020-03-16" class="text-gray-500">Sep 18, 2023</time>
           <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Web Development</a>
