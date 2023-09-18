7676e78d470e0e48963a40a795bdd31bc92c3dd7
KANABOINA VIGNESH <123869203+Vignesh18280@users.noreply.github.com>
2023-09-18 08:25:24 +0000

    made additional changes in freelance page

--- src/components/GetFreelance.jsx
+++ src/components/GetFreelance.jsx
@@ -10,7 +10,7 @@ function Getfreelance() {
       <p class="mt-2 text-lg leading-8 text-gray-600">So, what are you waiting for?seize the opportunity, unleash your potential, and let your freelance journey begin with a single, electrifying choice here!</p>
     </div>
     <div class="mx-auto mt-10 grid max-w-2xl grid-cols-1 gap-x-8 gap-y-16 border-t border-gray-200 pt-10 sm:mt-16 sm:pt-16 lg:mx-0 lg:max-w-none lg:grid-cols-3">
-      <article class="flex max-w-xl flex-col items-start justify-between">
+      <article class="flex max-w-xl flex-col border-2 border-white p-3 rounded-md items-start justify-between">
         <div class="flex items-center gap-x-4 text-xs">
           <time datetime="2020-03-16" class="text-gray-500">Sep 18, 2023</time>
           <a href="#" class="relative z-10 rounded-full bg-gray-50 px-3 py-1.5 font-medium text-gray-600 hover:bg-gray-100">Web Development</a>
