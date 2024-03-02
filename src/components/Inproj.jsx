b2a988ddcf39903027551e7b7bf2b50189921da1
nikhilbhatia08 <bhatianikhil551@gmail.com>
2024-03-02 09:36:38 +0530

    whatever

--- src/components/Inproj.jsx
+++ src/components/Inproj.jsx
@@ -40,9 +40,13 @@ function Inproj(props) {
                 </div>
                 <div className="py-4 flex-col border-b">
                     <div className=""><h1 className="text-white sm:text-xs md:text-xs lg:text-4xl">Video</h1></div>
-                    <div className="py-3 md-h-40"><iframe className="mx-auto lg:w-[540px] md-w-[200px] sm:w-[310px] lg:h-96 sm:h-[240px] md:h-40" 
-                        src="https://www.youtube.com/embed/WtkKwO1viI8?si=igjLzL4sI_08nHM8" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" allowfullscreen>
-                    </iframe></div>
+                    <video width="750" height="500" autoplay >
+                    <source src={project.video_url} type="video/mp4"/>
+                    </video>
+                </div>
+                <div className="py-4 flex-col border-b">
+                    <div className=""><h1 className="text-white sm:text-xs md:text-xs lg:text-4xl">Documents</h1></div>
+                    <Link class="inline-flex gap-x-2 text-gray-400 hover:text-gray-200" to = {project.decumentation}>Click Here</Link>
                 </div>
                 <div className="py-4 flex-col border-b">
                     <div className=""><p className="text-4xl text-white">Architecture</p></div>
