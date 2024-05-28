864f5eb5f94be05d434dd4f6e2b0543b1e147f06
nikhilbhatia08 <bhatianikhil551@gmail.com>
2024-05-28 15:01:03 +0530

    change in base url

--- src/components/utils.js
+++ src/components/utils.js
@@ -1,2 +1,2 @@
-export const BASE_URL = 'https://serverforcollab-production.up.railway.app'
+export const BASE_URL = 'https://serverforcollab-production-6b80.up.railway.app'
 //export const BASE_URL = 'http://localhost:5050'
